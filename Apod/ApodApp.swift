f185fe42722c58b97c4470fa224a7f03f37ff6dc
SchwiftyUI <schwiftyui@gmail.com>
2020-12-07 21:25:00 -0800

    Initial Commit

---
+++ Apod/ApodApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApodApp.swift
+//  Apod
+//
+//  Created by SchwiftyUI on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct ApodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
